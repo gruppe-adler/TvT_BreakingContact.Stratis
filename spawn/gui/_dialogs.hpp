a60911c3173098fbd0e4421527a9767954db6a24
nomisum <nomisum@googlemail.com>
2016-01-22 17:41:49 +0100

    Update _dialogs.hpp with btr60

--- spawn/gui/_dialogs.hpp
+++ spawn/gui/_dialogs.hpp
@@ -164,7 +164,7 @@ class russianSupplyGUI
 				y = 3.5 * GUI_GRID_H + GUI_GRID_Y;
 				w = 7 * GUI_GRID_W;
 				h = 6 * GUI_GRID_H;
-				text = "spawn\gui\pic\rus_btr70.paa";
+				text = "spawn\gui\pic\rus_btr60.paa";
 			};
 
 			class russian_gui_pic_6: RscPicture
