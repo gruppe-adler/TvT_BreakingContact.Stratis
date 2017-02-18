fc413aca35e367028506f34dd0abb755a96b379b
nomisum <nomisum@googlemail.com>
2017-02-18 02:30:33 +0100

    marker update class was missing

--- grad_tracking/cfgFunctions.hpp
+++ grad_tracking/cfgFunctions.hpp
@@ -14,6 +14,7 @@ class GRAD_tracking {
         class listenerTicks {};
         class radioVehMarkerAnimation {};
         class showIntervalWarning {};
+        class showMarkerUpdateHint {};
         class showTicksInstant {};
         class showTicksWarning {};
         class terminalAddAction {};
