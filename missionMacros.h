37809d499afc0f7051cb0bfa7df0d7b442a257b3
nomisum <nomisum@googlemail.com>
2018-02-07 21:56:58 +0100

    added opt loading pic, removed global debug msg

--- missionMacros.h
+++ missionMacros.h
@@ -1,2 +1,2 @@
-#define debugLog(msg) if (DEBUG_MODE) then { [msg, "GRAD_mission_debug", [true, true, true] ] call CBA_fnc_debug; }
+#define debugLog(msg) if (DEBUG_MODE) then { [msg, "GRAD_mission_debug", [true, true, false] ] call CBA_fnc_debug; }
 #define adminLog(msg) [msg, "GRAD_mission_admin", [DEBUG_MODE, true, false] ] call CBA_fnc_debug
