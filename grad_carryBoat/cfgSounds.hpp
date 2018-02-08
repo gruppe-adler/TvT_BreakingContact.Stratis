1b83c83aaeb8d0ae0f29cf50b25c253b281f992e
Simon Schmidt <simon.schmidt@idealo.de>
2018-02-08 09:15:12 +0100

    moved carryboat sounds to dir

---
+++ grad_carryBoat/cfgSounds.hpp
@@ -0,0 +1,64 @@
+class pick
+{
+  name = "pick";
+  // filename, volume, pitch
+  sound[] = {"sounds\pick.ogg", 1, 1};
+  // subtitle delay in seconds, subtitle text
+  titles[] = {0, ""};
+};
+class strain1
+{
+  name = "strain1";
+  // filename, volume, pitch
+  sound[] = {"sounds\strain1.ogg", 1, 1};
+  // subtitle delay in seconds, subtitle text
+  titles[] = {0, ""};
+};
+class strain2
+{
+  name = "strain2";
+  // filename, volume, pitch
+  sound[] = {"sounds\strain2.ogg", 1, 1};
+  // subtitle delay in seconds, subtitle text
+  titles[] = {0, ""};
+};
+class strain3
+{
+  name = "strain3";
+  // filename, volume, pitch
+  sound[] = {"sounds\strain3.ogg", 1, 1};
+  // subtitle delay in seconds, subtitle text
+  titles[] = {0, ""};
+};
+class strain4
+{
+  name = "strain4";
+  // filename, volume, pitch
+  sound[] = {"sounds\strain4.ogg", 1, 1};
+  // subtitle delay in seconds, subtitle text
+  titles[] = {0, ""};
+};
+class release_easy
+{
+  name = "release_easy";
+  // filename, volume, pitch
+  sound[] = {"sounds\release_easy.ogg", 1, 1};
+  // subtitle delay in seconds, subtitle text
+  titles[] = {0, ""};
+};
+class release_medium
+{
+  name = "release_medium";
+  // filename, volume, pitch
+  sound[] = {"sounds\release_medium.ogg", 1, 1};
+  // subtitle delay in seconds, subtitle text
+  titles[] = {0, ""};
+};
+class release_hard
+{
+  name = "release_hard";
+  // filename, volume, pitch
+  sound[] = {"sounds\release_hard.ogg", 1, 1};
+  // subtitle delay in seconds, subtitle text
+  titles[] = {0, ""};
+};
