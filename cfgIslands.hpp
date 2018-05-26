345a531b210c31278c861731b5bbe728638e901b
nomisum <nomisum@googlemail.com>
2018-05-26 13:56:55 +0200

    added ruha to cfgislands

--- cfgIslands.hpp
+++ cfgIslands.hpp
@@ -304,6 +304,16 @@ class CfgIslands {
         previewCamTarget[] = {3853,4930,0};
     };
 
+    class ruha {
+        displayName = "Ruha";
+        type = "european";
+        isWoodland = true;
+        spawnPosBlu[] = {0,0,0};
+        spawnPosOpf[] = {8284,8104,0};
+        previewCamPos[] = {3939,4968,0};
+        previewCamTarget[] = {3853,4930,0};
+    };
+
     class Stratis {
         displayName = "Stratis";
         type = "mediterranean";
