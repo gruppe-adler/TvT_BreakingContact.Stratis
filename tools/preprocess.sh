6c68c19c84c025f9c5a6c65c61db34723a608f34
Salbei <salbei@gruppe-adler.de>
2019-01-08 21:43:53 +0100

    lint

--- tools/preprocess.sh
+++ tools/preprocess.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DIRECTORY=`dirname $0`/..
-
-while getopts 'd:' OPTION; do
+PREPARE_ONLY="no"
+while getopts 'd:p' OPTION; do
   case "$OPTION" in
     d)
       DIRECTORY=${OPTARG%"/"}
@@ -10,8 +10,11 @@ while getopts 'd:' OPTION; do
     m)
       MACRODIR=${OPTARG%"/"}
       ;;
+    p)
+      PREPARE_ONLY="yes"
+      ;;
     ?)
-      echo "script usage: $(basename $0) [-d project_directory] [-m macro_target_dir]" >&2
+      echo "script usage: $(basename $0) [-d project_directory] [-m macro_target_dir] [-p]" >&2
       exit 1
       ;;
   esac
@@ -49,27 +52,46 @@ downloadMacroLib ${cbadir} "http://gruppe-adler.de/api/travis/cba.tar.gz"
 downloadMacroLib ${acedir} "http://gruppe-adler.de/api/travis/ace.tar.gz"
 downloadMacroLib ${a3dir} "http://gruppe-adler.de/api/travis/a3.tar.gz"
 
-INCLUDINGFILES=`grep -lire '^\s*#include '`
+FILESWITHINCLUDES=`grep -lire '^\s*#include '`
 
-echo "INFO editing #include clauses: forward-slashes, correct macro lib include paths…"
+if [[ PREPARE_ONLY == "no" ]]; then
+    echo "INFO editing #include clauses: \\ => / as path joiner"
+    for FILEWITHINCLUDE in ${FILESWITHINCLUDES}; do
+        sed -i '/#include/s/\\/\//g' "$FILEWITHINCLUDE" #forward-slash all the paths
+    done
+fi
 
-for INCLUDEFILE in ${INCLUDINGFILES}; do
-    sed -i '/#include/s/\\/\//g' "$INCLUDEFILE" #forward-slash all the paths
-    sed -i '/#include/s/\/A3/'$(escapeSlashesForSed ${a3dir})'/' "$INCLUDEFILE"
-    sed -i '/#include/s/\/x\/cba/'$(escapeSlashesForSed ${cbadir})'/' "$INCLUDEFILE"
-    sed -i '/#include/s/\/x\/grad\///' "$INCLUDEFILE" #special case for gruppe_adler_mod
-    sed -i '/#include/s/\/z\/ace/'$(escapeSlashesForSed ${acedir})'/' "$INCLUDEFILE"
-done
-echo "INFO removing illegal double-hash from macro files…"
-for MACROFILE in `find . -iname '*.cpp' -or -iname '*.hpp' -or -iname '*.h' -or -iname '*.inc'`
-do
-    sed -i -e 's/##//g' "$MACROFILE"
+echo "INFO editing #include clauses: correct include paths…"
+for FILEWITHINCLUDE in ${FILESWITHINCLUDES}; do
+    sed -i '/#include/s/\/[Aa]3\(.*\)/'$(escapeSlashesForSed ${a3dir})'\L\1/' "$FILEWITHINCLUDE" #lower-case the a3-include
+    sed -i '/#include/s/\/x\/cba/'$(escapeSlashesForSed ${cbadir})'/' "$FILEWITHINCLUDE"
+    sed -i '/#include/s/\/x\/grad\///' "$FILEWITHINCLUDE" #special case for gruppe_adler_mod
+    sed -i '/#include/s/\/x\/grad_captivWalkingAnimation\///' "$FILEWITHINCLUDE" #special case for grad_captivWalkingAnimation
+    sed -i '/#include/s/\/z\/ace/'$(escapeSlashesForSed ${acedir})'/' "$FILEWITHINCLUDE"
 done
 
-echo "INFO starting preprocessing of SQF files…"
-for SQFFILE in `find . -iname '*.sqf'`
-do
-    cpp -iquote ./ "$SQFFILE" "${SQFFILE}.1" && mv "${SQFFILE}.1" "$SQFFILE"
-done
+
+if [[ PREPARE_ONLY == "no" ]]; then
+    echo "INFO removing illegal double-hash from macro files…"
+    for MACROFILE in `find . \( -iname '*.cpp' -or -iname '*.hpp' -or -iname '*.h' -or -iname '*.inc' \) -and -not -type l`
+    do
+        sed -i -e 's/##//g' "$MACROFILE"
+    done
+
+    echo "INFO starting preprocessing of SQF files…"
+    for SQFFILE in `find . -iname '*.sqf'`
+    do
+        cpp -iquote ./ "$SQFFILE" "${SQFFILE}.1" && mv "${SQFFILE}.1" "$SQFFILE"
+    done
+
+    # remove hash stuff at top of file, it confuses the hell out of sqflint
+    for SQFFILE in `find . -iname '*.sqf'`
+    do
+        sed -i -E 's/^\s*#.*//g' "$SQFFILE"
+    done
+
+else
+    echo "INFO skipping actual preprocessing…"
+fi
 
 popd
