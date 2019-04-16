80e7050bc7928b447bbd76f494a40b0493b30618
nomisum <nomisum@googlemail.com>
2019-04-16 20:39:51 +0200

    add hat carrier blufor

--- BC_loadouts/factions/usa.hpp
+++ BC_loadouts/factions/usa.hpp
@@ -120,20 +120,7 @@ class USA {
             };
         };
 
-        // todo is this class actually used
-        class Soldier_AAT_F: Soldier_F {
-            backpack = "B_Carryall_khk";
-            addItemsToVest[] = {
-                LIST_8("rhs_mag_30Rnd_556x45_M855A1_Stanag"),
-                LIST_2("rhsusf_mag_15Rnd_9x19_JHP"),
-                LIST_2("rhs_mag_m67"),
-                LIST_2("SmokeShell")
-            };
-            addItemsToBackpack[] = {
-                LIST_2("rhsusf_200rnd_556x45_M855_mixed_box")
-            };
-        };
-
+        // heavy at
         class Soldier_AT_F: Soldier_F {
             secondaryWeapon = "rhs_weap_maaws";
             secondaryWeaponMagazine = "rhs_mag_maaws_HEAT";
@@ -152,6 +139,16 @@ class USA {
             };
             addItemsToBackpack[] = {
                 "rhs_mag_maaws_HEAT"
+            };
+        };
+
+        // heavy at ammo carrier
+        class Soldier_AAT_F: Soldier_F {
+            addItemsToBackpack[] = {
+                LIST_6("ACE_fieldDressing"),
+                LIST_2("ACE_epinephrine"),
+                LIST_2("ACE_morphine"),
+                LIST_2("rhs_mag_maaws_HEAT")
             };
         };
 
