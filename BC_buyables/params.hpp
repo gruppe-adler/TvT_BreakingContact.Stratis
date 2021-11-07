2ec9926182be9cbcf62201f21dd76fe237ee3f67
nomisum <nomisum@gmail.com>
2021-11-07 11:22:50 +0100

    reset params to classic

--- BC_buyables/params.hpp
+++ BC_buyables/params.hpp
@@ -3,7 +3,7 @@ class BUYABLES_BLUFOR
     title = "Blufor";
     values[] = {0, 1, 2, 3, 4};
     texts[] = {"bwGM", "USA", "BW Tanks", "sovjetGM", "USVN"};
-    default = 4;
+    default = 1;
 };
 
 class BUYABLES_OPFOR
@@ -11,5 +11,5 @@ class BUYABLES_OPFOR
     title = "Opfor";
     values[] = {0, 1, 2, 3, 4, 5};
     texts[] = {"nvaGM", "Russia", "Russia Tanks", "mudscha", "Rebels", "PAVN"};
-    default = 5;
+    default = 1;
 };
